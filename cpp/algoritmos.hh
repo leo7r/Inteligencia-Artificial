cc40f77c5ed094042e9fd8d3463b8e218da48ddf
RubenSerradas <rubenserradas@gmail.com>
2014-04-21 14:47:58 -0430

    Algoritmo ida_start

---
+++ cpp/algoritmos.hh
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ida_start.hh
+ *
+ *    Description:  Headers para ida_start
+ *
+ *        Version:  1.0
+ *        Created:  21/04/14 14:23:05
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+void ida_start(Node,int (*h)());
+
+int search(Node,int,Node);
