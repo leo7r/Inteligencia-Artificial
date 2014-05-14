84d5a95b37bd0bbbc07b265c79c4586cf2b145f3
RubenSerradas <rubenserradas@gmail.com>
2014-05-13 23:05:27 -0430

    Headers para la tablita

--- hash_table.h
+++ hash_table.h
@@ -3,7 +3,7 @@
  *
  *       Filename:  hash_table.h
  *
- *    Description:  Tabla de hash. Todavia no hay nada
+ *    Description:  Tabla de hash.
  *
  *        Version:  1.0
  *        Created:  07/05/14 19:09:56
@@ -15,4 +15,29 @@
  *
  * =====================================================================================
  */
+#include <stdio.h>
+#include <gmp.h> // Libreria para numeros GRANDES. https://gmplib.org/ 
+#include "state.h"
+#include <limits.h>
+#include <stdint.h>
+mpz_t factorial16;
 
+typedef struct{
+
+} hash_table16;
+
+/**
+ * Algoritmo para asociar un numero a una permutacion 
+ * Revisar "Ranking and unranking permutations in linear time"
+ */
+mpz_t rank(int, int_fast64_t , int_fast64_t);
+
+/**
+ * Algoritmo para obtener una permutacion de un numero determinado 
+ * Revisar "Ranking and unranking permutations in linear time"
+ */
+void unrank(int, mpz_t, int_fast64_t*);
+
+state* get(hash_table16*,int_fast64_t); /* Obtengo un estado a partir de la permutacion */
+
+int put(hash_table16*,state*);          /* Inserto un estado en la tabla */
