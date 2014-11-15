24d5ec424ee1aed8b22c0d91f00dfd70e904101b
bernardojm91 <bernardojm91@gmail.com>
2014-11-15 16:15:15 -0430

    Listo con resultados y respuestas para el informe

--- IA 2/Proyecto 2/clasificador_iris.py
+++ IA 2/Proyecto 2/clasificador_iris.py
@@ -7,6 +7,7 @@ from deap import creator, base, tools, algorithms
 import pdb
 RULE_SIZE = 24
 EJEMPLOS = []
+RULE_LIMIT = 3 # None en caso de no querer esto
 
 def mensaje_ayuda():
         """Imprime mensaje de ayuda"""
@@ -121,12 +122,15 @@ def comparar(ejemplo,individuo):
             return True
 
         return False
-                        
+                 
 def fitness(individual):
     """Retorna el fitness de un individuo segun la siguiente formula:
             fitness(individual) = (porcentaje aciertos) ** 2
     """
     global EJEMPLOS
+
+    if RULE_LIMIT and len(individual)/RULE_SIZE > RULE_LIMIT:
+        return 0,
 
     fitness = 0
     for ejemplo in EJEMPLOS:
@@ -139,6 +143,7 @@ def fitness(individual):
 
     #if fitness != 0:
     #    pdb.set_trace()
+
 
     return (fitness / float(len(EJEMPLOS)))**2,
 
@@ -184,6 +189,14 @@ def crossOver(ind1,ind2):
     return ( creator.Individual(newsT[0]) , creator.Individual(newsT[1]) )
     #return newsT
 
+def seleccion_padres( offspring , population , tipo ):
+
+    for i in range(5):
+        rnd = random.randint(0,len(offspring)-1)
+        rnd2 = random.randint(0,len(population)-1)
+
+        offspring[ rnd ] = population[ i if tipo == 'elitismo' else rnd2 ]
+
 def main():
     """Funcion principal de nuestro programa"""
     global EJEMPLOS
@@ -193,8 +206,8 @@ def main():
     #Agrego argumentos
     parser.add_argument("-e","--entrenamiento",type=str,help="Conjunto de datos de entrenamiento")
     parser.add_argument("-f","--prueba",type=str,help="Conjunto de datos de prueba")
-    parser.add_argument("-p","--padres",type=str,help="Metodo de seleccion de padres",choices=["ruleta","elitismo"])
-    parser.add_argument("-s","--sobrevivientes",type=str,help="Metodo de seleccion de sobrevivientes",choices=["ruleta","torneo"])
+    parser.add_argument("-p","--padres",type=str,help="Metodo de seleccion de padres",choices=["ruleta","mejor"], default = 'ruleta')
+    parser.add_argument("-s","--sobrevivientes",type=str,help="Metodo de seleccion de sobrevivientes",choices=["elitismo","ruleta"] , default = 'elitismo')
 
     arg = parser.parse_args()
     
@@ -216,28 +229,31 @@ def main():
     toolbox.register("evaluate",fitness)
     toolbox.register("mate", crossOver2 )
     toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-    toolbox.register("select", tools.selBest, k=5)
+
+    if arg.padres == 'ruleta':
+        toolbox.register("select", tools.selRoulette, k=5)
+    else:
+        toolbox.register("select", tools.selBest, k=5)
 
     population = toolbox.population(n=100)
+    print 'Empezando A.G con seleccion de padres %s, sobrevivientes %s' % (arg.padres,arg.sobrevivientes)
 
     NGEN=500
     for gen in range(NGEN):
         #print 'Generacion %s' % gen
         #print mediaFitness(population)**(1/2.0)
 
-        offspring = algorithms.varAnd(population, toolbox, cxpb=0.8, mutpb=0.1)
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.8, mutpb=0.05)
 
         # Elitismo
         population.sort( key = lambda ind: fitness(ind) , reverse = True )
-        print "Fitness =  %s | Poblacion %s" % (fitness(population[0])[0]**(1/2.0),len(population))
+        print "Fitness =  %s | Generacion = %s" % (fitness(population[0])[0]**(1/2.0),gen)
 
         if fitness(population[0])[0] == 1.0:
             print "Num gen %s" % gen
             break
 
-        for i in range(5):
-            rnd = random.randint(0,len(offspring)-1)
-            offspring[ rnd ] = population[i]
+        seleccion_padres(offspring,population,arg.sobrevivientes)
 
         fits = toolbox.map(toolbox.evaluate, offspring)
         for fit, ind in zip(fits, offspring):
@@ -247,6 +263,129 @@ def main():
 
     population.sort( key = lambda ind: fitness(ind) , reverse = True )
     print population[0]
+    print len(population[0])/RULE_SIZE
+
+    pruebas = leer_datos(arg.prueba)
+    buenas = 0
+    malas = 0
+    for prueba in pruebas:
+        if comparar(prueba,population[0]):
+            buenas+=1
+        else:
+            malas+=1
+    print 'Buenas %s | Malas %s | Porcentaje exito = %s' % ( buenas,malas,(buenas/float(buenas+malas))*100 )
+
+def main_prueba():
+
+    """Funcion principal de nuestro programa"""
+    global EJEMPLOS
+
+    #Creo el parser de argumentos
+    parser = argparse.ArgumentParser(description='Clasificador de Iris usando un sistema tipo GABIL.') 
+    #Agrego argumentos
+    parser.add_argument("-e","--entrenamiento",type=str,help="Conjunto de datos de entrenamiento")
+    parser.add_argument("-f","--prueba",type=str,help="Conjunto de datos de prueba")
+    parser.add_argument("-p","--padres",type=str,help="Metodo de seleccion de padres",choices=["ruleta","mejor"], default = 'ruleta')
+    parser.add_argument("-s","--sobrevivientes",type=str,help="Metodo de seleccion de sobrevivientes",choices=["elitismo","ruleta"] , default = 'elitismo')
+
+    arg = parser.parse_args()
+    
+    if (arg.entrenamiento == None or arg.prueba == None):
+            print "Faltan los archivos de entrenamiento o de prueba. Usa el argumento -h para obtener instrucciones."
+            return
+
+    EJEMPLOS = leer_datos(arg.entrenamiento) 
+    
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list , fitness=creator.FitnessMax)
+    
+    toolbox = base.Toolbox()
+
+    toolbox.register("attr_bool", random.randint, 0, 1)
+    toolbox.register("individual", initGabilI, creator.Individual, toolbox.attr_bool,24)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    
+    toolbox.register("evaluate",fitness)
+    toolbox.register("mate", crossOver2 )
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+
+    if arg.padres == 'ruleta':
+        toolbox.register("select", tools.selRoulette, k=5)
+    else:
+        toolbox.register("select", tools.selBest, k=5)
+
+    '''
+    AQUI SE HACEN LAS PRUEBAS PARA CADA VARIACION
+    '''
+
+    tasas_mutacion = [ 0.02 , 0.05 , 0.1 ]
+    tasas_crossover = [ 0.1 , 0.4 , 0.8 ]
+    penalizaciones = [ None , 10 , 20 ]
+    num_repeticiones = 100
+    resultados = []
+
+    for mut in tasas_mutacion:
+        for cross in tasas_crossover:
+            for pen in penalizaciones:
+
+                population = toolbox.population(n=100)
+                offspring = []
+                prom_res = 0
+                RULE_LIMIT = pen
+
+                for rep in range(num_repeticiones):
+
+                    NGEN=500
+                    for gen in range(NGEN):
+                        #print 'Generacion %s' % gen
+                        #print mediaFitness(population)**(1/2.0)
+
+                        offspring = algorithms.varAnd(population, toolbox, cxpb=cross, mutpb=mut)
+
+                        # Elitismo
+                        population.sort( key = lambda ind: fitness(ind) , reverse = True )
+                        #print "Fitness =  %s | Generacion = %s" % (fitness(population[0])[0]**(1/2.0),gen)
+
+                        if fitness(population[0])[0] == 1.0:
+                            #print "Num gen %s" % gen
+                            break
+
+                        seleccion_padres(offspring,population,arg.sobrevivientes)
+
+                        fits = toolbox.map(toolbox.evaluate, offspring)
+                        for fit, ind in zip(fits, offspring):
+                            ind.fitness.values = fit
+
+                        population = offspring
+
+                    population.sort( key = lambda ind: fitness(ind) , reverse = True )
+                    #print population[0]
+                    #print len(population[0])/RULE_SIZE
+
+                    pruebas = leer_datos(arg.prueba)
+                    buenas = 0
+                    malas = 0
+                    for prueba in pruebas:
+                        if comparar(prueba,population[0]):
+                            buenas+=1
+                        else:
+                            malas+=1
+
+                    prom_res+= (buenas/float(buenas+malas))*100
+
+                rrr = {
+                    'Tipo' : 'Mut: %s | CX: %s | Pen: %s' % (mut,cross,pen),
+                    'Resultado' : prom_res/num_repeticiones
+                    }
+                resultados.append(rrr)
+                print rrr
+                    #print 'Buenas %s | Malas %s | Porcentaje exito = %s' % ( buenas,malas,(buenas/float(buenas+malas))*100 )
+    #print resultados
+    with open('resultados_pruebas','w+') as f:
+        f.write('Se hicieron 100 corridas para cada una de las configuraciones, estos son los resultados:\n')
+        for resultado in resultados:
+            f.write( str(resultado) + '\n' )
 
 if __name__ == "__main__":
     main()
+    #main_prueba()
